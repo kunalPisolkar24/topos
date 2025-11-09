5c4b5a6fd2cd5c84c1dc1ed646298478a846563b
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-11-09 13:18:50 +0000

    update cors to accept patch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ const app = new Hono<{
 
 app.use('*', cors({
   origin: '*',
-  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowMethods: ['GET', 'POST','PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowHeaders: ['Content-Type', 'Authorization'],
 }));
 
