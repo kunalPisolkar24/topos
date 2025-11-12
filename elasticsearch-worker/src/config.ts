45856d99e2922e2e27e36be38b8c7516e3763b3c
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-11-12 05:54:11 +0000

    map elastic worker to internal listener port

--- elasticsearch-worker/src/config.ts
+++ elasticsearch-worker/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const KAFKA_BROKER = process.env.KAFKA_BROKER || 'kafka:9092';
+export const KAFKA_BROKER = process.env.KAFKA_BROKER || 'kafka:29092';
 export const POSTS_TOPIC = 'posts';
 export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL || 'http://elasticsearch:9200';
 export const PORT = parseInt(process.env.PORT || '8002', 10);
