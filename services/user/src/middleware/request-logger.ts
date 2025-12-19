5e89e750004d2aec42e0a2ab7d5628e6b533a2a4
kunalPisolkar24 <kpisolkar24@gmail.com>
2025-12-19 23:42:26 +0530

    add logger

---
+++ services/user/src/middleware/request-logger.ts
@@ -0,0 +1,33 @@
+import { Context, Next } from 'hono';
+import { logger } from '../lib/logger';
+
+export const requestLogger = async (c: Context, next: Next) => {
+    const start = Date.now();
+    const { method, url } = c.req;
+    const userAgent = c.req.header('user-agent');
+    const requestId = crypto.randomUUID();
+
+    c.set('requestId', requestId);
+
+    logger.info({
+        msg: 'Incoming Request',
+        method,
+        url,
+        requestId,
+        userAgent
+    });
+
+    await next();
+
+    const duration = Date.now() - start;
+    const status = c.res.status;
+
+    logger.info({
+        msg: 'Request Completed',
+        method,
+        url,
+        status,
+        duration: `${duration}ms`,
+        requestId
+    });
+};
