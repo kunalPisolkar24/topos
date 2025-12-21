d57874e63c71db0ed34f1d3fdb333cc8b597e158
kunalPisolkar24 <kpisolkar24@gmail.com>
2025-12-21 12:04:06 +0530

    implement interfaces for tags

---
+++ services/content/internal/repository/mongo_tag.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/kunalPisolkar24/blogapp/services/content/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type mongoTagRepo struct {
+	collection *mongo.Collection
+}
+
+func NewMongoTagRepository(db *mongo.Database) domain.TagRepository {
+	return &mongoTagRepo{
+		collection: db.Collection("tags"),
+	}
+}
+
+func (r *mongoTagRepo) CreateOrFind(ctx context.Context, name string) (*domain.Tag, error) {
+	filter := bson.M{"name": name}
+	update := bson.M{"$setOnInsert": bson.M{"name": name}}
+	opts := options.FindOneAndUpdate().SetUpsert(true).SetReturnDocument(options.After)
+
+	var tag domain.Tag
+	err := r.collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&tag)
+	if err != nil {
+		return nil, err
+	}
+	return &tag, nil
+}
+
+func (r *mongoTagRepo) FindAll(ctx context.Context) ([]*domain.Tag, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var tags []*domain.Tag
+	if err = cursor.All(ctx, &tags); err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
