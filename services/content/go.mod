189b437dbf2edc08559ebec492b8afe21a5219ad
kunalPisolkar24 <kpisolkar24@gmail.com>
2025-12-21 11:07:54 +0530

    setup env

--- services/content/go.mod
+++ services/content/go.mod
@@ -4,6 +4,7 @@ go 1.25.5
 
 require (
 	github.com/99designs/gqlgen v0.17.85
+	github.com/joho/godotenv v1.5.1
 	github.com/vektah/gqlparser/v2 v2.5.31
 )
 
---
+++ services/content/internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	MongoURI  string
+	DbName    string
+	JwtSecret string
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	return &Config{
+		Port:      getEnv("PORT", "4002"),
+		MongoURI:  getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		DbName:    getEnv("DB_NAME", "blog_content"),
+		JwtSecret: getEnv("JWT_SECRET", "secret"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
