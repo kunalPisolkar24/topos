b4ea6bda77d67dbb2f2e0b3dd9cd97a24b81511f
kunalPisolkar24 <kpisolkar24@gmail.com>
2025-12-25 10:52:44 +0530

    add search in elastic interface

--- services/search/src/core/interfaces/repository.interface.ts
+++ services/search/src/core/interfaces/repository.interface.ts
@@ -1,7 +1,8 @@
-import { PostDocument } from '../entities/post.entity.js';
+import { PostDocument, SearchResult } from '../entities/post.entity.js';
 
 export interface IElasticsearchRepository {
   upsert(document: PostDocument): Promise<void>;
   delete(id: string): Promise<void>;
+  search(query: string, page: number, limit: number): Promise<SearchResult>;
   checkHealth(): Promise<boolean>;
 }
