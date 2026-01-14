84cbae8e3004a87d555d9141f6e49f2a640ac7e2
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-14 23:44:55 +0530

    update prisma mock

--- services/user/tests/mocks/prisma.ts
+++ services/user/tests/mocks/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../src/generated/prisma/client';
 import { mockDeep, DeepMockProxy } from 'vitest-mock-extended';
 
 export const prismaMock = mockDeep<PrismaClient>();
 
-export default prismaMock;
-
 export type PrismaMock = DeepMockProxy<PrismaClient>;
+
+export default prismaMock;
