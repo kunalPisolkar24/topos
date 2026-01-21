24e49a74f0d5786f81e62cadd71b788658cfdc18
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-21 17:14:28 +0530

    Add ContextRequest, TagsResponse, PostGenerationRequest, and PostGenerationResponse message types. Implement GenerateTags and GeneratePost methods in AIService, enhancing content generation capabilities.

--- services/content/proto/ai/ai_service.pb.go
+++ services/content/proto/ai/ai_service.pb.go
@@ -109,6 +109,214 @@ func (x *ContentResponse) GetSummary() string {
 	return ""
 }
 
+type ContextRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Title         string                 `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
+	Body          string                 `protobuf:"bytes,2,opt,name=body,proto3" json:"body,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *ContextRequest) Reset() {
+	*x = ContextRequest{}
+	mi := &file_proto_ai_ai_service_proto_msgTypes[2]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ContextRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ContextRequest) ProtoMessage() {}
+
+func (x *ContextRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_ai_ai_service_proto_msgTypes[2]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ContextRequest.ProtoReflect.Descriptor instead.
+func (*ContextRequest) Descriptor() ([]byte, []int) {
+	return file_proto_ai_ai_service_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ContextRequest) GetTitle() string {
+	if x != nil {
+		return x.Title
+	}
+	return ""
+}
+
+func (x *ContextRequest) GetBody() string {
+	if x != nil {
+		return x.Body
+	}
+	return ""
+}
+
+type TagsResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Tags          []string               `protobuf:"bytes,1,rep,name=tags,proto3" json:"tags,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *TagsResponse) Reset() {
+	*x = TagsResponse{}
+	mi := &file_proto_ai_ai_service_proto_msgTypes[3]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *TagsResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TagsResponse) ProtoMessage() {}
+
+func (x *TagsResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_ai_ai_service_proto_msgTypes[3]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TagsResponse.ProtoReflect.Descriptor instead.
+func (*TagsResponse) Descriptor() ([]byte, []int) {
+	return file_proto_ai_ai_service_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *TagsResponse) GetTags() []string {
+	if x != nil {
+		return x.Tags
+	}
+	return nil
+}
+
+type PostGenerationRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Prompt        string                 `protobuf:"bytes,1,opt,name=prompt,proto3" json:"prompt,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *PostGenerationRequest) Reset() {
+	*x = PostGenerationRequest{}
+	mi := &file_proto_ai_ai_service_proto_msgTypes[4]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *PostGenerationRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PostGenerationRequest) ProtoMessage() {}
+
+func (x *PostGenerationRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_ai_ai_service_proto_msgTypes[4]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PostGenerationRequest.ProtoReflect.Descriptor instead.
+func (*PostGenerationRequest) Descriptor() ([]byte, []int) {
+	return file_proto_ai_ai_service_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *PostGenerationRequest) GetPrompt() string {
+	if x != nil {
+		return x.Prompt
+	}
+	return ""
+}
+
+type PostGenerationResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Title         string                 `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
+	Body          string                 `protobuf:"bytes,2,opt,name=body,proto3" json:"body,omitempty"`
+	Summary       string                 `protobuf:"bytes,3,opt,name=summary,proto3" json:"summary,omitempty"`
+	Tags          []string               `protobuf:"bytes,4,rep,name=tags,proto3" json:"tags,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *PostGenerationResponse) Reset() {
+	*x = PostGenerationResponse{}
+	mi := &file_proto_ai_ai_service_proto_msgTypes[5]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *PostGenerationResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PostGenerationResponse) ProtoMessage() {}
+
+func (x *PostGenerationResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_ai_ai_service_proto_msgTypes[5]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PostGenerationResponse.ProtoReflect.Descriptor instead.
+func (*PostGenerationResponse) Descriptor() ([]byte, []int) {
+	return file_proto_ai_ai_service_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *PostGenerationResponse) GetTitle() string {
+	if x != nil {
+		return x.Title
+	}
+	return ""
+}
+
+func (x *PostGenerationResponse) GetBody() string {
+	if x != nil {
+		return x.Body
+	}
+	return ""
+}
+
+func (x *PostGenerationResponse) GetSummary() string {
+	if x != nil {
+		return x.Summary
+	}
+	return ""
+}
+
+func (x *PostGenerationResponse) GetTags() []string {
+	if x != nil {
+		return x.Tags
+	}
+	return nil
+}
+
 var File_proto_ai_ai_service_proto protoreflect.FileDescriptor
 
 const file_proto_ai_ai_service_proto_rawDesc = "" +
@@ -117,9 +325,23 @@ const file_proto_ai_ai_service_proto_rawDesc = "" +
 	"\x0eContentRequest\x12\x12\n" +
 	"\x04text\x18\x01 \x01(\tR\x04text\"+\n" +
 	"\x0fContentResponse\x12\x18\n" +
-	"\asummary\x18\x01 \x01(\tR\asummary2G\n" +
+	"\asummary\x18\x01 \x01(\tR\asummary\":\n" +
+	"\x0eContextRequest\x12\x14\n" +
+	"\x05title\x18\x01 \x01(\tR\x05title\x12\x12\n" +
+	"\x04body\x18\x02 \x01(\tR\x04body\"\"\n" +
+	"\fTagsResponse\x12\x12\n" +
+	"\x04tags\x18\x01 \x03(\tR\x04tags\"/\n" +
+	"\x15PostGenerationRequest\x12\x16\n" +
+	"\x06prompt\x18\x01 \x01(\tR\x06prompt\"p\n" +
+	"\x16PostGenerationResponse\x12\x14\n" +
+	"\x05title\x18\x01 \x01(\tR\x05title\x12\x12\n" +
+	"\x04body\x18\x02 \x01(\tR\x04body\x12\x18\n" +
+	"\asummary\x18\x03 \x01(\tR\asummary\x12\x12\n" +
+	"\x04tags\x18\x04 \x03(\tR\x04tags2\xc4\x01\n" +
 	"\tAIService\x12:\n" +
-	"\x0fGenerateSummary\x12\x12.ai.ContentRequest\x1a\x13.ai.ContentResponseB>Z<github.com/kunalPisolkar24/blogapp/services/content/proto/aib\x06proto3"
+	"\x0fGenerateSummary\x12\x12.ai.ContentRequest\x1a\x13.ai.ContentResponse\x124\n" +
+	"\fGenerateTags\x12\x12.ai.ContextRequest\x1a\x10.ai.TagsResponse\x12E\n" +
+	"\fGeneratePost\x12\x19.ai.PostGenerationRequest\x1a\x1a.ai.PostGenerationResponseB>Z<github.com/kunalPisolkar24/blogapp/services/content/proto/aib\x06proto3"
 
 var (
 	file_proto_ai_ai_service_proto_rawDescOnce sync.Once
@@ -133,16 +355,24 @@ func file_proto_ai_ai_service_proto_rawDescGZIP() []byte {
 	return file_proto_ai_ai_service_proto_rawDescData
 }
 
-var file_proto_ai_ai_service_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_proto_ai_ai_service_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_proto_ai_ai_service_proto_goTypes = []any{
-	(*ContentRequest)(nil),  // 0: ai.ContentRequest
-	(*ContentResponse)(nil), // 1: ai.ContentResponse
+	(*ContentRequest)(nil),         // 0: ai.ContentRequest
+	(*ContentResponse)(nil),        // 1: ai.ContentResponse
+	(*ContextRequest)(nil),         // 2: ai.ContextRequest
+	(*TagsResponse)(nil),           // 3: ai.TagsResponse
+	(*PostGenerationRequest)(nil),  // 4: ai.PostGenerationRequest
+	(*PostGenerationResponse)(nil), // 5: ai.PostGenerationResponse
 }
 var file_proto_ai_ai_service_proto_depIdxs = []int32{
 	0, // 0: ai.AIService.GenerateSummary:input_type -> ai.ContentRequest
-	1, // 1: ai.AIService.GenerateSummary:output_type -> ai.ContentResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 1: ai.AIService.GenerateTags:input_type -> ai.ContextRequest
+	4, // 2: ai.AIService.GeneratePost:input_type -> ai.PostGenerationRequest
+	1, // 3: ai.AIService.GenerateSummary:output_type -> ai.ContentResponse
+	3, // 4: ai.AIService.GenerateTags:output_type -> ai.TagsResponse
+	5, // 5: ai.AIService.GeneratePost:output_type -> ai.PostGenerationResponse
+	3, // [3:6] is the sub-list for method output_type
+	0, // [0:3] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -159,7 +389,7 @@ func file_proto_ai_ai_service_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_proto_ai_ai_service_proto_rawDesc), len(file_proto_ai_ai_service_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
